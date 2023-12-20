1ea4cd241b08093721e8d0d28d51d7a391f2b67d
Brilliant-Atosam <atosam91@gmail.com>
2023-12-20 08:09:07 +0000

    savings portfolio api

---
+++ config.js
@@ -0,0 +1,3 @@
+export const VERIFICATION_TOKEN = "this is something that does not concern you";
+export const mail_password = "vbtw yqum yztz xkan";
+export const mail_email = "mycashlens@gmail.com";
