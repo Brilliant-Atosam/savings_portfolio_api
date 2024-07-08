029dd9503d406e3715dd72da6f1515803178f59b
Brilliant-Atosam <atosam91@gmail.com>
2024-07-08 10:29:07 +0100

    cashlens debt management feature

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ const app = express();
 
 app.use(cors());
 cron.schedule("0 0 1 * *", () => updateNotifications());
-// updateNotifications();
 app.get("/", async (req, res) => {
   const users = await User.find();
   res.json(users);
