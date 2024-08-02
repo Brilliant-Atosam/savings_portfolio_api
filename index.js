3a6030010d598d495bbf68bc3bffc5c9b2e25d12
Brilliant-Atosam <atosam91@gmail.com>
2024-08-02 07:39:02 +0100

    cron job

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ import updateNotifications from "./notifications.js";
 const app = express();
 app.use(cors());
 cron.schedule("0 0 1 * *", () => updateNotifications());
+updateNotifications();
 app.get("/", async (req, res) => {
   const users = await User.find();
   res.json(users);
